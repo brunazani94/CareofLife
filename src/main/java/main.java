7a75d562d51294e814f52b9ea5791e67c1b261d6
brunazani94 <brunazani_94@hotmail.com>
2023-11-16 19:32:22 -0300

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,10 @@
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,8 +14,9 @@
  *
  * @author Bruna
  */
-public class main {
+public class Main {
      public static void main(String[] args) {
          
      }
-}
+         
+     }
